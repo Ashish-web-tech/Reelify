903d0d59074b1a663abb3ef7955eb614b8722ff7
Ashish Mishra <akonwtf8@gmail.com>
2025-07-03 16:24:32 +0530

    Update Hero.jsx

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ export const Hero = () => {
     }
 
     try {
-      const response = await fetch("https://reelify-backend-xjn1.onrender.comget-reel", {
+      const response = await fetch("https://reelify-backend-xjn1.onrender.com/get-reel", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url }),
